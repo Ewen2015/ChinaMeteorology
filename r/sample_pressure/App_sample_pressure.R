d42866ac8f763805238b2ce5fa22be55f9fbb6fa
Ewen <wang.enqun@outlook.com>
2016-05-26 02:25:06 -0600

    Adds sample pressure.

---
+++ r/sample_pressure/App_sample_pressure.R
@@ -0,0 +1,53 @@
+#
+# This is a Shiny web application. You can run the application by clicking
+# the 'Run App' button above.
+#
+# Find out more about building applications with Shiny here:
+#
+#    http://shiny.rstudio.com/
+#
+
+setwd("/Users/ewenwang/Dropbox/Data Science/ChinaMeteorology/data")
+
+library(shiny)
+library(leaflet)
+
+# load data and preprocess data
+data = read.csv("sample_pressure.csv", header = T)
+data = data[-c(which(data$averagePressure == 999999)),]
+
+data$averagePressure = data$averagePressure/10
+data$popup = paste("站台站区号:", data$stationID, ";",
+                   "累年年平均本站气压:", data$averagePressure, "hPa")
+data$averagePressure = scale(data$averagePressure, center = T)
+
+data$lat = data$lat/100
+data$long = data$long/100
+
+# build the app
+r_colors <- rgb(t(col2rgb(colors()) / 255))
+names(r_colors) <- colors()
+
+ui <- fluidPage(
+  leafletOutput("mymap"),
+  p(),
+  actionButton("recalc", "New points")
+)
+
+server <- function(input, output, session) {
+  
+  points <- eventReactive(input$recalc, {
+    cbind(lng=data$long, lat=data$lat)
+  }, ignoreNULL = FALSE)
+  
+  output$mymap <- renderLeaflet({
+    leaflet(data) %>% addTiles() %>%
+      addCircles(lng = ~long, lat = ~lat, weight = 1,
+                 radius = ~averagePressure*100000, popup = ~popup
+      )
+  })
+}
+
+# run app
+shinyApp(ui, server)
+
